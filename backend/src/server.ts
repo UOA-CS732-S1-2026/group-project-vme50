b9585da70b7b1be7651638e3fd0a42cc68f81819
Reign2xgoaway <enai775@aucklanduni.ac.nz>
2026-04-14 05:08:29 +1200

    Initialize Repository
    Setup frontend and backend.

---
+++ backend/src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Platemates API running");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend is working!" });
+});
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
