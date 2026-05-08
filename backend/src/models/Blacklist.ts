fa2cb22919c6776d6fb6e140b5b88c109589f3d8
Reign2xgoaway <enai775@aucklanduni.ac.nz>
2026-05-09 11:11:12 +1200

    Updated Blacklist Model
    Automatically removes stored expired token in the database.

--- backend/src/models/Blacklist.ts
+++ backend/src/models/Blacklist.ts
@@ -1,8 +1,21 @@
 import mongoose from "mongoose";
 
-const blacklistSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  expiresAt: { type: Date, required: true },
-});
+const blacklistSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+      expires: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export default mongoose.model("Blacklist", blacklistSchema);
