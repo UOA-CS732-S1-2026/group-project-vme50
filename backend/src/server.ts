45ac91860ff5aa5241376a338c7e0a5f8e98d531
Reign2xgoaway <enai775@aucklanduni.ac.nz>
2026-05-10 10:52:05 +1200

    Project Deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,9 +13,12 @@ const app = express();
 /* ---------------- Middleware ---------------- */
 app.use(express.json());
 
+const allowedOrigin = process.env.CLIENT_URL || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigin,
+    credentials: true,
   }),
 );
 
