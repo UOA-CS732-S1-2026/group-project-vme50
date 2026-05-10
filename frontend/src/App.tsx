ad95282b7470e58cd28aa065508a4951aa11d8ce
Reign2xgoaway <enai775@aucklanduni.ac.nz>
2026-05-11 06:08:22 +1200

    Implemented NotFound Page
    This is a page for non-existent URLs.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 import ProtectedRoute from "./components/redirect/ProtectedRoute";
 import PageTitle from "./hooks/usePageTitle";
@@ -27,6 +28,9 @@ function App() {
               </ProtectedRoute>
             }
           />
+
+          {/* NOT FOUND */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </PageTitle>
     </BrowserRouter>
