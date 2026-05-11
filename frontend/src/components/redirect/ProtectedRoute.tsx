577c862fd123010f21891c7a4740d91cd58b9eb9
xilu128 <xilu128@aucklanduni.ac.nz>
2026-05-11 21:34:32 +1200

    Replace main frontend with Frontend-Scaffold UI

--- frontend/src/components/redirect/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import type { ReactElement } from "react";
-import { isAuthenticated } from "../../utils/auth.util";
-
-const ProtectedRoute = ({ children }: { children: ReactElement }) => {
-  return isAuthenticated() ? children : <Navigate to="/" replace />;
-};
-
-export default ProtectedRoute;
